625e7150f70d3331cf35102e067e0d618d112b62
s17001 <s17001@std.it-college.ac.jp>
2019-06-04 12:28:43 +0900

    Storeからユーザーを管理できる下地を整えた

---
+++ store/index.js
@@ -0,0 +1,38 @@
+export const state = () => ({
+  isLoggedIn: false ,
+  user: null
+})
+
+export const getters = {
+  isLoggedIn: (state) => state.isLoggedIn,
+  user: (state) => state.user
+}
+
+export const mutations = {
+  setUser(state, {user}){
+    state.user = user
+    state.isLoggedIn = true
+  }
+}
+
+export const actions = {
+  async login({ commit }, { id }) {
+    const user = await this.$axios.$get(`/users/${id}.json`)
+
+    if (!user) {throw new Error('Invalid user')}
+
+    commit('setUser', { user })
+  },
+  async register({ commit }, { id }) {
+    const payload = {}
+    payload[id] = { id }
+
+    await this.$axios.$patch(`/users.json`,payload)
+
+    const user = await this.$axios.$get(`/users/${id}.json`)
+
+    if (!user) {throw new Error('Invalid user')}
+
+    commit('setUser', { user })
+  }
+}
