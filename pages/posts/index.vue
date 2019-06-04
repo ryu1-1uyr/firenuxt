9bab21a2da2f021aee387a4ddea10d85041dffd5
s17001 <s17001@std.it-college.ac.jp>
2019-06-04 15:03:22 +0900

    indexの更新

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,59 +1,90 @@
 <template>
-  <section class="container posts-page">
+  <section class="container">
     <el-card style="flex: 1">
       <div slot="header" class="clearfix">
-        <span>新着投稿</span>
+        <span>ログイン</span>
       </div>
-      <el-table
-        :data="showPosts"
-        style="width: 100%"
-        class="table"
-      >
-        <el-table-column
-          prop="title"
-          label="タイトル">
-        </el-table-column>
-        <el-table-column
-          prop="user.id"
-          label="投稿者"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="created_at"
-          label="投稿日時"
-          width="240">
-        </el-table-column>
-      </el-table>
+      <form>
+        <div class="form-content">
+          <span>ユーザー ID</span>
+          <el-input placeholder="" v-model="formData.id" />
+        </div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
+        </div>
+        <div class="text-right">
+          <el-button type="primary" @click="handleClickSubmit">{{buttonText}}</el-button>
+        </div>
+      </form>
     </el-card>
   </section>
 </template>
 
 <script>
+  import { mapGetters, mapActions } from 'vuex'
+
   export default {
-    computed: {
-      showPosts() {
-        return [
-          {
-          id: '001',
-          title: 'これはタイトルです',
-          body: '心理的安全を確保していこうな',
-          created_at: '2019/06/04 11:00:00',
-          user: {
-            id: 'ryu1__1uyr'
-          }
-        },
-          {
-            id: '002',
-            title: 'これもタイトルです',
-            body: 'は〜〜〜〜〜〜〜感情〜〜〜〜〜',
-            created_at: '2019/06/04 11:00:00',
-            user: {
-              id: '†ryu†'
-            }
-          },
-        ]
-      },
+    asyncData({ redirect, store }) {
+      if (store.getters['user']) {
+        redirect('/posts/')
+      }
+      return {
+        isCreateMode: false,
+        formData: {
+          id: ''
+        }
+      }
     },
+    computed: {
+      buttonText() {
+        return this.isCreateMode ? '新規登録' : 'ログイン'
+      },
+      ...mapGetters(['user'])
+    },
+    methods: {
+      async handleClickSubmit() {
+        if (this.isCreateMode) {
+          try {
+            await this.register({ ...this.formData })
+            this.$notify({
+              type: 'success',
+              title: 'アカウント作成完了',
+              message: `${this.formData.id} として登録しました`,
+              position: 'bottom-right',
+              duration: 1000
+            })
+            this.$router.push('/posts/')
+          } catch (e) {
+            this.$notify.error({
+              title: 'アカウント作成失敗',
+              message: '既に登録されているか、不正なユーザー ID です',
+              position: 'bottom-right',
+              duration: 1000
+            })
+          }
+        } else {
+          try {
+            await this.login({ ...this.formData })
+            this.$notify({
+              type: 'success',
+              title: 'ログイン成功',
+              message: `${this.formData.id} としてログインしました`,
+              position: 'bottom-right',
+              duration: 1000
+            })
+            this.$router.push('/posts/')
+          } catch (e) {
+            this.$notify.error({
+              title: 'ログイン失敗',
+              message: '不正なユーザー ID です',
+              position: 'bottom-right',
+              duration: 1000
+            })
+          }
+        }
+      },
+      ...mapActions(['login', 'register'])
+    }
   }
 </script>
 
