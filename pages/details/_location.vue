33a805f0cb1b35092f01c7448e06d58f8336baec
s17001 <s17001@std.it-college.ac.jp>
2019-06-06 17:22:38 +0900

    クエリパラメーターから座標をとってくるやつにした

--- pages/details/_location.vue
+++ pages/details/_location.vue
@@ -1,9 +1,21 @@
 <template>
-    
+    <div>
+      詳細の画面です
+      {{this.location.latitude}}
+      {{this.location.longitude}}
+    </div>
 </template>
 
 <script>
     export default {
+      data () {
+        return {
+          location: {
+            latitude: this.$nuxt.$route.params.location.split(',')[0] ,
+            longitude: this.$nuxt.$route.params.location.split(',')[1]
+          }
+        }
+      },
         name: "_location"
     }
 </script>
