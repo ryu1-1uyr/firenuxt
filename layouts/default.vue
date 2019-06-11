37dc2bca062ca3ed032c0d7db3d0c31b80799f50
s17001 <s17001@std.it-college.ac.jp>
2019-06-11 19:19:50 +0900

    でふぉるとにふぃればせから取ったデータをのっけてみた

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,10 +13,42 @@
 <script>
   import TheHeader from '~/components/TheHeader.vue'
   import Postdata from '~/components/Postdata'
+  import firebase from '~/plugins/firebase'
+  import { mapGetters, mapActions ,mapState } from 'vuex'
+
   export default {
+    data(){
+      return {
+        userlist:[]
+      }
+    },
     components: {
       TheHeader,
       Postdata
-    }
+    },
+    methods: {
+      testlog(){
+
+        // this.$store.commit('listElement/addList',{element:this.userlist})
+
+        let test = this.$store.state.list
+        console.log(test)
+
+      },
+    },
+    computed: {
+
+      loadList () {
+        const db =  firebase.firestore()
+        db.collection("users").get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.userlist.push(doc.data());
+          });
+          this.$store.commit('setList', this.userlist )
+        })
+
+      },
+      ...mapGetters(['user']),
+    },
   }
 </script>
