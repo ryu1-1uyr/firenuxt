db269c5f3c9da32caedac27d8e90dd4b198dd941
s17001 <s17001@std.it-college.ac.jp>
2019-06-11 14:57:58 +0900

    googlemapを埋め込んだよ

--- pages/details/_location.vue
+++ pages/details/_location.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-      詳細の画面です
-      {{this.location.latitude}}
-      {{this.location.longitude}}
+
+      <div>
+        <div style="background-color: black;width: 450px;height: 600px">hoge</div>
+      </div>
+
+      <div>
+        <iframe :src=basedata width="900" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
     </div>
 </template>
 
@@ -10,6 +16,7 @@
     export default {
       data () {
         return {
+          basedata:`https://maps.google.co.jp/maps?output=embed&t=m&hl=ja&z=18&ll=${this.$nuxt.$route.params.location.split(',')[0]},${this.$nuxt.$route.params.location.split(',')[1]}`,
           location: {
             latitude: this.$nuxt.$route.params.location.split(',')[0] ,
             longitude: this.$nuxt.$route.params.location.split(',')[1]
