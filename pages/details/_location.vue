1c272babb2c01f9528be03248ade58c174862091
s17001 <s17001@std.it-college.ac.jp>
2019-06-13 15:10:06 +0900

    根本的にデータの呼び出し方を変更した

--- pages/details/_location.vue
+++ pages/details/_location.vue
@@ -4,9 +4,9 @@
 
       <div>
         <div style="background-color: azure;width: 450px;height: 600px;margin-right: 50px">
-          <span v-if="JSON.stringify(this.$store.state.list[this.elementNumber])">
-          <p>{{JSON.stringify(this.$store.state.list[this.elementNumber]['comment'])}}</p>
-            <p> {{JSON.stringify(this.$store.state.list[this.elementNumber]['image'])}}</p>
+          <span v-if="detailsJSON">
+          <p>{{JSON.stringify(detailsJSON['comment'])}}</p>
+            <p>latitude : {{detailsJSON.latitude}} , longitude : {{detailsJSON.longitude}}</p>
             <img v-if="detailsJSON" :src="detailsJSON.image" alt="うつってへんよ">
             </span>
         </div>
@@ -20,12 +20,19 @@
 </template>
 
 <script>
+  let tmp
+
     export default {
-      asyncData({ store }) {
-        let hoge = store.getters['getList']
-        console.log("a",hoge)
+      asyncData({ store ,route }) {
+        tmp = store.state.list[route.params.location.split(',')[2]]
+        // console.log(tmp,route.params.location.split(',')[2])
 
       },
+      // asyncData({ store }) {
+      //   let hoge = store.getters['getList']
+      //   console.log("a",hoge)
+      //
+      // },
       data () {
         return {
           basedata:`https://maps.google.co.jp/maps?output=embed&t=m&hl=ja&z=18&ll=${this.$nuxt.$route.params.location.split(',')[0]},${this.$nuxt.$route.params.location.split(',')[1]}`,
@@ -34,12 +41,12 @@
             longitude: this.$nuxt.$route.params.location.split(',')[1]
           },
           elementNumber:this.$nuxt.$route.params.location.split(',')[2],
-          detailsJSON: {}
+          detailsJSON: tmp
         }
       },
       methods:{
         test(){
-          this.detailsJSON = JSON.stringify(this.$store.state.list[this.elementNumber])
+          // this.detailsJSON = JSON.stringify(this.$store.state.list[this.elementNumber])
           console.log(this.detailsJSON)
         }
       },
